6a4fc97613f593d54e5b0a8edd3862a2ba4e2261
Asce <vchuynhchihieu@gmail.com>
2024-09-16 21:28:02 +0700

    add error handle

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 
+const APIError = require("./app/api-error");
 const contactRouter = require('./app/routes/contact.route');
 
 const app = express();
@@ -14,6 +15,18 @@ app.use('/api/contacts', contactRouter);
 app.get('/', (req, res) => {
     res.json({
         message: 'Welcome to contact book application.',
+    });
+});
+
+// Handle 404 response
+app.use((req, res, next) => {
+    return next(new APIError(404, 'Resource not found'));
+});
+
+// Middleware handle errors
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal Server Error',
     });
 });
 
